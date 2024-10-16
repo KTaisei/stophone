246df7c1a1d25b87fa54d4563c69671f9c76181f
KTaisei <mathlike.science@gmail.com>
2024-10-16 14:27:00 +0900

    loggerの追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:flutter_background/flutter_background.dart' as fb;
-import 'package:logging/logging.dart';
+import 'package:logger/logger.dart'; // ログ用パッケージ
 
 void main() {
-  // ログの設定を行う
-  _setupLogging();
-
   runApp(
     const MaterialApp(
       home: WalkingPhoneApp(),
     ),
   );
-}
-
-// ログ設定関数
-void _setupLogging() {
-  // すべてのログレベル（ALL）を記録
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((LogRecord record) {
-    // ここでログの表示方法をカスタマイズ
-    // 例: コンソールに表示、またはファイルに保存
-    debugPrint(
-        '${record.level.name}: ${record.time}: ${record.loggerName}: ${record.message}');
-  });
 }
 
 class WalkingPhoneApp extends StatefulWidget {
@@ -37,7 +22,8 @@ class WalkingPhoneApp extends StatefulWidget {
 }
 
 class WalkingPhoneAppState extends State<WalkingPhoneApp> {
-  final Logger _logger = Logger('WalkingPhoneAppState'); // ロガーインスタンス
+  final Logger _logger = Logger(); // ロガーインスタンス
+
   double _speed = 0.0;
   late StreamSubscription<Position> _positionSubscription;
   late StreamSubscription<UserAccelerometerEvent> _accelerometerSubscription;
@@ -50,7 +36,8 @@ class WalkingPhoneAppState extends State<WalkingPhoneApp> {
   void initState() {
     super.initState();
     _initializeBackgroundTask();
-    _initializeLocationService();
+    _checkLocationService(); // 新しいサービスチェック関数
+    _requestLocationPermission(); // 許可をリクエストする部分を分離
     _startAccelerometer();
     _startScreenTimer();
   }
@@ -59,7 +46,7 @@ class WalkingPhoneAppState extends State<WalkingPhoneApp> {
   void _initializeBackgroundTask() async {
     const androidConfig = fb.FlutterBackgroundAndroidConfig(
       notificationTitle: "Walking Phone Alert",
-      notificationText: "あなたの移動を監視中...",
+      notificationText: "Monitoring your movement...",
       notificationImportance: fb.AndroidNotificationImportance.high,
       notificationIcon: fb.AndroidResource(
         name: 'background_icon',
@@ -73,117 +60,125 @@ class WalkingPhoneAppState extends State<WalkingPhoneApp> {
           await fb.FlutterBackground.initialize(androidConfig: androidConfig);
       if (success) {
         fb.FlutterBackground.enableBackgroundExecution();
-        _logger.info('バックグラウンド実行が有効化されました');
-      } else {
-        _logger.warning('バックグラウンド実行の有効化に失敗しました');
+        _logger.i("Background execution enabled.");
       }
     }
   }
 
-  // 位置情報サービスの初期化
-  void _initializeLocationService() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  // 位置情報サービスの確認
+  void _checkLocationService() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      _logger.severe('位置情報サービスが無効です');
-      return Future.error('Location services are disabled.');
+      _logger.w("Location services are disabled.");
+      return;
     }
+    _initializeLocationService();
+  }
 
-    permission = await Geolocator.checkPermission();
+  // 位置情報許可のリクエストと確認
+  void _requestLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        _logger.warning('位置情報の許可が拒否されました');
-        return Future.error('Location permissions are denied');
+        _logger.w("Location permissions are denied.");
+        return;
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      _logger.severe('位置情報の許可が永久に拒否されています');
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
+      _logger.e("Location permissions are permanently denied.");
+      return;
     }
 
-    _positionSubscription =
-        Geolocator.getPositionStream().listen((Position position) {
-      double speedKmh = position.speed * 3.6;
-      setState(() {
-        _speed = speedKmh;
-      });
-
-      _checkConditions();
-      _logger.info('現在の速度: ${speedKmh.toStringAsFixed(2)} km/h');
-    });
+    _logger.i("Location permission granted.");
+    _initializeLocationService();
   }
 
-  void _startAccelerometer() {
-    _accelerometerSubscription = userAccelerometerEventStream().listen((event) {
-      double calculatedSpeed = _calculateSpeed(event);
-      _logger
-          .info('Calculated Accelerometer Speed: $calculatedSpeed'); // デバッグ用ログ
-
-      setState(() {
-        _isMovingAtWalkingSpeed = calculatedSpeed > 0;
-      });
-
-      _checkConditions();
-    });
-  }
-
-  double _calculateSpeed(UserAccelerometerEvent event) {
-    double magnitude =
-        sqrt(event.x * event.x + event.y * event.y + event.z * event.z);
-    return magnitude;
-  }
-
-  void _startScreenTimer() {
-    _screenTimer?.cancel(); // タイマーをリセット
-    _screenOnForMoreThanOneMinute = false; // 初期状態をリセット
-    _screenTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (timer.tick >= 60) {
+  // 位置情報サービスの初期化と監視
+  void _initializeLocationService() {
+    _positionSubscription = Geolocator.getPositionStream().listen(
+      (Position position) {
+        double speedKmh = position.speed * 3.6;
         setState(() {
-          _screenOnForMoreThanOneMinute = true;
+          _speed = speedKmh;
         });
-      }
-    });
+
+        _logger.i("Current speed: $_speed km/h");
+        _checkConditions();
+      },
+      onError: (e) {
+        _logger.e("Error in location stream: $e");
+      },
+    );
   }
 
+  // 加速度センサーの初期化
+  void _startAccelerometer() {
+    _accelerometerSubscription = userAccelerometerEvents.listen(
+      (event) {
+        double calculatedSpeed = _calculateSpeed(event);
+        _isMovingAtWalkingSpeed = calculatedSpeed > 0;
+
+        _logger.i("Accelerometer event: ${event.toString()}");
+        _checkConditions();
+      },
+    );
+  }
+
+  // 加速度から速度を計算
+  double _calculateSpeed(UserAccelerometerEvent event) {
+    return sqrt(event.x * event.x + event.y * event.y + event.z * event.z);
+  }
+
+  // 画面タイマーの開始
+  void _startScreenTimer() {
+    _screenTimer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        if (timer.tick >= 60) {
+          setState(() {
+            _screenOnForMoreThanOneMinute = true;
+          });
+          _logger.i("Screen has been on for more than one minute.");
+        }
+      },
+    );
+  }
+
+  // 条件チェック
   void _checkConditions() {
     if (_isMovingAtWalkingSpeed &&
         _speed >= 3.0 &&
         _speed <= 5.0 &&
         _screenOnForMoreThanOneMinute) {
-      // 速度が3~5km/hかつ画面が1分以上点灯している場合に警告
       _showWarningScreen();
-    } else if (_speed >= 1.0 && _speed < 3.0) {
-      // 速度が1~3km/hの場合は警告を出さない
-      return;
-    } else if (_speed >= 3.0 &&
-        _speed <= 5.0 &&
-        !_screenOnForMoreThanOneMinute) {
-      // 速度が3~5km/hだが、画面が1分以上点灯していない場合も警告を出さない
-      return;
     }
   }
 
+  // 警告画面の表示
   void _showWarningScreen() {
     setState(() {
-      _warningCount++; // 警告が表示されるたびにカウントを増やす
+      _warningCount++;
     });
 
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => const WarningScreen(),
-      fullscreenDialog: true,
-    ));
+    _logger.w("Warning count: $_warningCount");
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const WarningScreen(),
+        fullscreenDialog: true,
+      ),
+    );
   }
 
+  // リソースの解放
   @override
   void dispose() {
     _positionSubscription.cancel();
     _accelerometerSubscription.cancel();
-    _screenTimer?.cancel(); // タイマーもキャンセル
+    _screenTimer?.cancel();
     fb.FlutterBackground.disableBackgroundExecution();
     super.dispose();
   }
@@ -202,13 +197,7 @@ class WalkingPhoneAppState extends State<WalkingPhoneApp> {
             ),
             const SizedBox(height: 20),
             Text(
-              'Is Moving at Walking Speed: $_isMovingAtWalkingSpeed', // 移動状態を表示
-              style: const TextStyle(fontSize: 24),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Warning count: $_warningCount', // 警告回数の表示
+              'Warning count: $_warningCount',
               style: const TextStyle(fontSize: 24),
               textAlign: TextAlign.center,
             ),
